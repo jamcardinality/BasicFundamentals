69e33314c43bcc13f91364cdb32af3526905bcfc
jamcardinality <jamescarlcardinal@gmail.com>
2023-06-01 16:10:00 +0800

    Add arithmetic.js

---
+++ JS/arithmetic.js
@@ -0,0 +1,36 @@
+/*
+    Arithmetic Expression is a combination of
+    Operands (values, variables, etc.)
+    Operators (+ - * / %)
+    that can be evaluated to a value 
+    ex. y = x + 5
+*/
+
+let students = 20;
+
+// students = students + 1;
+// students = students - 1;
+// students = students * 1;
+// students = students / 1;
+// let extraStudents = students % 2;
+
+
+//Augmented Assignment Operator
+// students += 1;
+// students -= 1;
+// students *= 1;
+// students /= 1;
+
+// console.log(students);
+
+/*
+    Operator Precedence
+    1. Parenthesis ()
+    2. Exponents 
+    3. Multiplication & Division 
+    4. Addition & Substraction
+*/
+
+let result = (1 + 2) * (3 + 4);
+
+console.log(result);    
