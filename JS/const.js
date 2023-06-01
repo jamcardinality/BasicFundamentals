e32f4565e482d4f1d04bef9859518b9d8ee3961f
jamcardinality <jamescarlcardinal@gmail.com>
2023-06-01 16:57:10 +0800

    Add const.js

---
+++ JS/const.js
@@ -0,0 +1,12 @@
+// const = a variale that can't be changed
+
+const PI = 3.14159;
+let radius;
+let circumference;
+
+radius = window.prompt("Enter the radius of a circle");
+radius = Number(radius);
+
+circumference = 2 * pi * radius;
+
+console.log("The circumference is: ", circumference); 
