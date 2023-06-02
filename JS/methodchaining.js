6001329ec0e2080136bd5d0e72d9060405667186
jamcardinality <jamescarlcardinal@gmail.com>
2023-06-02 14:10:10 +0800

    Add Method Chaining

---
+++ JS/methodchaining.js
@@ -0,0 +1,7 @@
+//Method Chaining = calling one method after one another is one continuous line of code 
+
+let userName = "bro";
+
+let letter = userName.charAt(0).toUpperCase().trim();
+
+console.log(letter);
