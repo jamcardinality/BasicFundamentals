f0af94f5b9c74b4948c654ef27e8e8ab383b0bad
jamcardinality <jamescarlcardinal@gmail.com>
2023-06-02 20:34:29 +0800

    Add NOT Logical Operator

---
+++ JS/not.js
@@ -0,0 +1,21 @@
+// ! NOT logical Operator
+// Typically used to reverse a condition's boolean value
+// true -> false false -> true
+
+let temp = 15;
+let sunny = false;
+
+if(!(temp > 0)){
+    console.log("It's cold outside");
+}
+else{
+    console.log("It's warm outside");
+}
+
+if(!sunny){
+    console.log("It's cloudy outside");
+}
+else{
+    console.log("It's sunny outside");
+}
+
