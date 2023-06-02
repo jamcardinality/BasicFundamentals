5ada0716f7d26bb94144f3fcd9c36b544906b68a
jamcardinality <jamescarlcardinal@gmail.com>
2023-06-02 14:17:38 +0800

    Add if Statement

---
+++ JS/if.js
@@ -0,0 +1,31 @@
+// if Statement = a basic form of decision making if a condition is true, then do something if not, then don't do it!
+
+let age = 0;
+
+/*
+if(age >= 65){
+    console.log("You are a senior citizen");
+}
+
+else if(age >= 18){
+    console.log("You are a child");
+}
+
+else if(age < 0){
+    console.log("YOU HAVEN'T BEEN BORN YET");
+}
+
+else{
+    console.log("You are a child!");
+}
+
+*/
+
+let online = false;
+
+if(online){
+    console.log("You are online!");
+}
+else{
+    console.log("You are offline!");
+}
