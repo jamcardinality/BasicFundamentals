8f9f52a6b99a1c0f534a479c4e59f74ba2e5087f
jamcardinality <jamescarlcardinal@gmail.com>
2023-06-02 14:05:44 +0800

    slice() methods

---
+++ JS/slice.js
@@ -0,0 +1,14 @@
+//slice() extracts a section of a string and returns it as a new string, without modifiying the original string. 
+
+let fullName = "Bro Code";
+let firstName;
+let lastName; 
+
+// firstName = fullName.slice(0,3);
+// lastName = fullName.slice(4);
+
+firstName = fullName.slice(0,fullName.indexOf(" "));
+lastName = fullName.slice(fullName.indexOf(" ") + 1);
+
+console.log(firstName);
+console.log(lastName);
