69208e037d8bb06c476302bdbf36f28213e46300
jamcardinality <jamescarlcardinal@gmail.com>
2023-06-03 22:59:21 +0800

    Add Break and Continue

---
+++ JS/breakcontinue.js
@@ -0,0 +1,10 @@
+//break = breaks out of a loop entirely 
+//continue = skips an iteration of a loop
+
+for(let i = 1; i <= 20; i+=1){
+    if(i == 13){
+        continue;
+        //break;
+    }
+    console.log(i);
+}
