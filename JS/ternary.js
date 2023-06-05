72bac9aefc5585f4d75ad3015dba2ff7c7911378
jamcardinality <jamescarlcardinal@gmail.com>
2023-06-05 14:04:04 +0800

    Add Ternary Operator

---
+++ JS/ternary.js
@@ -0,0 +1,15 @@
+//Ternary Operator = shortcut for an ("if/else statement")
+//                  Take 3 Operands
+//
+//                  1. a condition witj ? 
+//                  2. if the expression if true :                     
+//                  3. if the expression if false 
+//
+// condition ? exprITtrue : exprIfFalse 
+
+checkWinner(false);
+
+function checkWinner(WIN){
+    WIN ? console.log("You are WIN!") : console.log("You are LOSE!");
+}
+
