040e86a07d827a7a4e9bdece7649725465a1db77
jamcardinality <jamescarlcardinal@gmail.com>
2023-06-05 14:10:34 +0800

    Add Variable Scope

---
+++ JS/variablescope.js
@@ -0,0 +1,7 @@
+// Variable Scope = where a variable is accessible
+// let = variables are limited to block scope {}
+// var = variable are limited to a function scope(){} 
+
+// global variable = is declared outside any function
+// (if global, var will CHANGE browser's window properties)
+
