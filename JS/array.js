d733c1bda2bdd6272c2a2c13e6e19a44c842a3e7
jamcardinality <jamescarlcardinal@gmail.com>
2023-06-06 16:09:27 +0800

    Add array.js

---
+++ JS/array.js
@@ -0,0 +1,15 @@
+ // array = think of it as a variable that can store multiple values
+
+ let fruits = ["apple", "orange", "banana"];
+
+//fruit[2] = "coconut";
+
+fruits.push("lemon"); // add an element
+fruits.pop(); //removes last element
+fruits.unshift("mango"); //add element to beginning
+fruits.shift(); //removes element from beginning 
+
+let length = fruits.length;
+let index = fruits.indexOf("kiwi");
+
+console.log(index);
