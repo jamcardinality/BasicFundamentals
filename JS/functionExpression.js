d714f1f0739d31219b725401e0d4462b5bbaafce
jamcardinality <jamescarlcardinal@gmail.com>
2023-06-15 14:53:16 +0800

    Add functionExpression

---
+++ JS/functionExpression.js
@@ -0,0 +1,28 @@
+// Function expression = function without a name (anonymous function) avoid polluting the global scope with names Write it, them forget about it.
+
+
+let count = 0;
+
+document.getElementById("increaseButton").onclick = function(){
+    count += 1;
+    document.getElementById("myLabel").innerHTML = count;
+}
+
+document.getElementById("decreaseButton").onclick = function(){
+    count -= 1;
+    document.getElementById("myLabel").innerHTML = count;
+}
+
+
+/*
+function increaseCount(){
+    count += 1;
+    document.getElementById("myLabel").innerHTML = count;
+}
+
+function decreaseButton(){
+    count -= 1;
+    document.getElementById("myLabel").innerHTML = count;
+}
+
+*/
