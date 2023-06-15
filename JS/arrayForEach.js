7cb24b224255bf1fb7040441ad8f9edd3fc9c797
jamcardinality <jamescarlcardinal@gmail.com>
2023-06-15 13:47:38 +0800

    Add arrayForEach

--- JS/arrayForEach.js
+++ JS/arrayForEach.js
@@ -7,7 +7,7 @@ students.forEach(print);
 function capitalize(element, index, array){
     array[index] = element[0].toUpperCase() + element.substring(1);
 }
-
+ 
 function print(element){
     console.log(element);
 }
