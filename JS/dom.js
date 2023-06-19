78f29c1961b621c9a25c90c399f4f33f81dc6d4a
jamcardinality <jamescarlcardinal@gmail.com>
2023-06-19 14:06:49 +0800

    Add DOM

---
+++ JS/dom.js
@@ -0,0 +1,13 @@
+// DOM = Document Object Model (API)
+//       An interface for changing the content of a page
+
+
+// console.log(document);
+// console.dir(document);
+// console.log(document.title);
+// console.log(document.URL);
+// document.title = "Title goes here";
+// document.location = "http://www.google.com";
+
+document.body.style.backgroundColor = "skyblue";
+document.getElementById("myDiv").innerHTML = "Hello";
