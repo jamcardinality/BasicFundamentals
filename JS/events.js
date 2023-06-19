fb69f0ace02d3fdd8bc3eebaba2a4e993bdf2577
jamcardinality <jamescarlcardinal@gmail.com>
2023-06-19 15:42:50 +0800

    Add Events

---
+++ JS/events.js
@@ -0,0 +1,26 @@
+// events =  an event is some action that the user or the browser does many HTML elements contain event attributes 
+
+// const element = document.getElementById("myButton");
+// const element = document.body;
+// const element = document.getElementById("myText");\
+const element = document.getElementById("myDiv");
+
+// element.onclick = doSomething;  
+// element.onload = doSomething;
+// element.onchange = doSomething;  
+// element.onmouseover = doSomething;
+// element.onmouseout = doSomethingElse;
+element.onmousedown = doSomething;
+element.onmouseup = doSomethingElse;
+
+
+
+function doSomething(){
+    //alert("You did something!");
+    element.style.backgroundColor = "red";
+}
+
+function doSomethingElse(){
+    //alert("You did something!");
+    element.style.backgroundColor = "lightgreen";
+}
